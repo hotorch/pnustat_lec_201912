cb5796d9a82b28ef94f304b47762456da8447fe8
Hoyoung Jeong <40910918+hotorch@users.noreply.github.com>
2019-12-25 19:23:08 +0900

    Update s3_4_MLP with MNIST.R

--- rscript/s3_4_MLP with MNIST.R
+++ rscript/s3_4_MLP with MNIST.R
@@ -1,17 +1,18 @@
-
-
+library(data.table)
 library(h2o)
 h2o.init(nthreads = -1)
 
 #  Load the MNIST and prepare the data ------------------------------------
 # This step takes a few seconds bc we have to download the data from the internet...
 # train_file <- "https://h2o-public-test-data.s3.amazonaws.com/bigdata/laptop/mnist/train.csv.gz"
-test_file <- "https://h2o-public-test-data.s3.amazonaws.com/bigdata/laptop/mnist/test.csv.gz"
 # train <- h2o.importFile(train_file)
-test <- h2o.importFile(test_file)
-dim(train);dim(test)
+# test_file <- "https://h2o-public-test-data.s3.amazonaws.com/bigdata/laptop/mnist/test.csv.gz"
+# test <- h2o.importFile(test_file)
 
-y <- "C785"  #response column: digits 0-9 / 1 ~ 784
+test_file <- fread('./data/mnist_test.csv', encoding = 'UTF-8')
+test <- as.h2o(test_file)
+
+y <- "V785"  #response column: digits 0-9 / 1 ~ 784
 x <- setdiff(names(test), y)  # vector of predictor column names
 
 # Since the response is encoded as integers, we need to tell H2O that
@@ -23,27 +24,27 @@ test[,y] <- as.factor(test[,y])
 
 # Modeling ----------------------------------------------------------------
 
-dl_fit1 <- h2o.deeplearning(x = x,
-                            y = y,
-                            training_frame = test,
-                            model_id = "dl_fit1",
-                            hidden = c(20,20),
-                            seed = 1)
-dl_fit1
+# dl_fit1 <- h2o.deeplearning(x = x,
+#                             y = y,
+#                             training_frame = test,
+#                             model_id = "dl_fit1",
+#                             hidden = c(20,20),
+#                             seed = 1)
+# dl_fit1
 
 # too much spend time
-dl_fit2 <- h2o.deeplearning(x = x,
-                            y = y,
-                            training_frame = test,
-                            model_id = "dl_fit2",
-                            epochs = 50,
-                            hidden = c(20,20),
-                            stopping_rounds = 0,  # disable early stopping
-                            seed = 1)
-dl_fit2
+# dl_fit2 <- h2o.deeplearning(x = x,
+#                             y = y,
+#                             training_frame = test,
+#                             model_id = "dl_fit2",
+#                             epochs = 50,
+#                             hidden = c(20,20),
+#                             stopping_rounds = 0,  # disable early stopping
+#                             seed = 1)
+# dl_fit2
 
 # recommend!!!
-dl_fit3 <- h2o.deeplearning(x = x,
+dl_fit3 <- h2o.deeplearning(x = x ,
                             y = y,
                             training_frame = test,
                             model_id = "dl_fit3",
@@ -91,50 +92,47 @@ plot(cv_models[[1]],
 
 
 # Deep Learning Grid Search -----------------------------------------------
-# do not run!! 
-activation_opt <- c("Rectifier", "Maxout")# , "Tanh")
-l1_opt <- c(0, 0.001)
-l2_opt <- c(0, 0.001)
-# l1_opt <- c(0, 0.00001, 0.0001, 0.001, 0.01)
-# l2_opt <- c(0, 0.00001, 0.0001, 0.001, 0.01)
 
-hyper_params <- list(activation = activation_opt, l1 = l1_opt, l2 = l2_opt)
-search_criteria <- list(strategy = "RandomDiscrete", max_runtime_secs = 600) # 10 min
+# activation_opt <- c("Rectifier", "Maxout")# , "Tanh")
+# l1_opt <- c(0, 0.001)
+# l2_opt <- c(0, 0.001)
+# # l1_opt <- c(0, 0.00001, 0.0001, 0.001, 0.01)
+# # l2_opt <- c(0, 0.00001, 0.0001, 0.001, 0.01)
 
-splits <- h2o.splitFrame(test, ratios = 0.8, seed = 1)
-
-dl_grid <- h2o.grid("deeplearning", x = x, y = y,
-                    grid_id = "dl_grid",
-                    training_frame = splits[[1]],
-                    validation_frame = splits[[2]],
-                    seed = 1,
-                    hidden = c(20,20),
-                    hyper_params = hyper_params,
-                    search_criteria = search_criteria)
-
-dl_gridperf <- h2o.getGrid(grid_id = "dl_grid", 
-                           sort_by = "accuracy", 
-                           decreasing = TRUE)
-print(dl_gridperf)
+# hyper_params <- list(activation = activation_opt, l1 = l1_opt, l2 = l2_opt)
+# search_criteria <- list(strategy = "RandomDiscrete", max_runtime_secs = 600) # 10 min
+# 
+# splits <- h2o.splitFrame(test, ratios = 0.8, seed = 1)
+# 
+# dl_grid <- h2o.grid("deeplearning", x = x, y = y,
+#                     grid_id = "dl_grid",
+#                     training_frame = splits[[1]],
+#                     validation_frame = splits[[2]],
+#                     seed = 1,
+#                     hidden = c(20,20),
+#                     hyper_params = hyper_params,
+#                     search_criteria = search_criteria)
+# 
+# dl_gridperf <- h2o.getGrid(grid_id = "dl_grid", 
+#                            sort_by = "accuracy", 
+#                            decreasing = TRUE)
+# print(dl_gridperf)
 
 
 
 # Best Model --------------------------------------------------------------
 
 # Grab the model_id for the top DL model, chosen by validation error.
-best_dl_model_id <- dl_gridperf@model_ids[[1]]
-best_dl <- h2o.getModel(best_dl_model_id)
-
-best_dl_perf <- h2o.performance(model = best_dl, newdata = test)
-best_dl_perf 
-
-h2o.mse(best_dl_perf)
+# best_dl_model_id <- dl_gridperf@model_ids[[1]]
+# best_dl <- h2o.getModel(best_dl_model_id)
+# 
+# best_dl_perf <- h2o.performance(model = best_dl, newdata = test)
+# best_dl_perf 
+# 
+# h2o.mse(best_dl_perf)
 
 # Reference ---------------------------------------------------------------
 # https://htmlpreview.github.io/?https://github.com/ledell/sldm4-h2o/blob/master/sldm4-deeplearning-h2o.html
 
-
 # h2o.shutdown(prompt = F)
 # end of document ---------------------------------------------------------
-
-
